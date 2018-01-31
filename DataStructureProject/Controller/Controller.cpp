96433ae4e609f21067f7ad8f67d848e2b139f937
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-01-31 12:22:08 -0700

    Added code in controller to print out timer info, added CrimeData files

--- DataStructureProject/Controller/Controller.cpp
+++ DataStructureProject/Controller/Controller.cpp
@@ -8,9 +8,22 @@
 
 #include "Controller.hpp"
 
+
 using namespace std;
 
 void Controller :: start()
 {
-    
+    Timer codeTimer;
+    codeTimer.startTimer();
+    cout << "wow code" << endl;
+    codeTimer.stopTimer();
+    codeTimer.displayInformation();
+    codeTimer.resetTimer();
+    codeTimer.startTimer();
+    for (int index = 0; index < 500000; index++)
+    {
+        cout << "Index is " << index << "\t";
+    }
+    codeTimer.stopTimer();
+    codeTimer.displayInformation();
 }
