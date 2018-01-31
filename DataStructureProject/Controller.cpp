9ad2888cf8ac1caf2e2bcdcfd979add8ed747ccd
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-01-31 11:34:28 -0700

    created controller and runner classes

---
+++ DataStructureProject/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ DataStructureProject/Runner.cpp
@@ -0,0 +1,9 @@
+//
+//  Runner.cpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <stdio.h>
