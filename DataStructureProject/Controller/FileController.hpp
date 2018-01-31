79a22eb42ff389f2f27fa8a3d13ce6194d769e43
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-01-31 12:28:54 -0700

    #includes

---
+++ DataStructureProject/Controller/FileController.hpp
@@ -0,0 +1,21 @@
+//
+//  FileController.hpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+using namespace std;
+
+#include <"./Data/CrimeData.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#endif /* FileController_hpp */
