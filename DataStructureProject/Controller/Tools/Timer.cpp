d8594eafd04bdfb0e993f0dc11e3804928e95aff
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-01-31 11:55:09 -0700

    Created Timer files, added timer hpp code

---
+++ DataStructureProject/Controller/Tools/Timer.cpp
@@ -0,0 +1,13 @@
+//
+//  Timer.cpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Timer.hpp"
+
+using namespace std;
+
+
