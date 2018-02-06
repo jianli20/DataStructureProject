436c33fa9b6d0c0ae7a9080379b1ae99d958d008
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-06 13:36:16 -0700

    Added Node.hpp

---
+++ DataStructureProject/Model/Nodes/Node.hpp
@@ -0,0 +1,49 @@
+//
+//  Node.hpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <stdio.h>
+
+template <class Type>
+class Node
+{
+protected:
+    Type data;
+public:
+    Node();
+    Node(Type data);
+    void setData(Type data);
+    Type getData();
+};
+
+template <class Type>
+Node<Type> :: Node()
+{
+    //Needed for default constructor complilation
+}
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->data = data;
+}
+
+template <class Type>
+void Node<Type> :: setData(Type data)
+{
+    this->data = data;
+}
+
+template <class Type>
+Type Node<Type> :: getData()
+{
+    return data;
+}
+
+#endif /* Node_hpp */
