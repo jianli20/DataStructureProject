97b59b1756a4a0137cc9bde720057f331435de21
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-20 11:45:08 -0700

    Added declarations, made the list LinkedList

--- DataStructureProject/Model/Structures/Linear/LinkedList.hpp
+++ DataStructureProject/Model/Structures/Linear/LinkedList.hpp
@@ -15,5 +15,36 @@ using namespace std;
 
 template <class Type>
 class LinkedList : public List<Type>
+{
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type> * end;
+public:
+    //Constructors
+    LinkedList();
+    //Destructor
+    virtual ~LinkedList(); // Virtual so it can be overriden
+    //Helper methods
+    int getSize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+    //Structure Methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    Type remove(int index);
+    // Type setAtIndex(int index, Type item);
+    // bool contains(Type item);
+    
+    LinkedList<Type> :: LinkedList()
+    {
+        this->front = nullptr;
+        this->end = nullptr;
+        this->size = 0;
+
+    }
+}
+
 
 #endif /* LinkedList_hpp */
