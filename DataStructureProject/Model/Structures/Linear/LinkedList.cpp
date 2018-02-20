bece62ff8cfe3cbf35d7910e79986d909972d563
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-20 11:40:48 -0700

    Created LinkedList class, added List.hpp include and std

---
+++ DataStructureProject/Model/Structures/Linear/LinkedList.cpp
@@ -0,0 +1,9 @@
+//
+//  LinkedList.cpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 2/20/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "LinkedList.hpp"
---
+++ DataStructureProject/Model/Structures/Linear/LinkedList.hpp
@@ -0,0 +1,16 @@
+//
+//  LinkedList.hpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 2/20/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "List.hpp"
+
+using namespace std;
+
+#ifndef LinkedList_hpp
+#define LinkedList_hpp
+
+#endif /* LinkedList_hpp */
