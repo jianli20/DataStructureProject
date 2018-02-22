e53ec0165d0d2b813ab8a7d698d8d8c48cc242d1
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-22 11:56:18 -0700

    Added virtual to structure methods, created circularlist, queue, and stack classes

---
+++ DataStructureProject/Model/Structures/Linear/CircularList.hpp
@@ -0,0 +1,14 @@
+//
+//  CircularList.hpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 2/22/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef CircularList_hpp
+#define CircularList_hpp
+
+#include <stdio.h>
+
+#endif /* CircularList_hpp */
---
+++ DataStructureProject/Model/Structures/Linear/Stack.hpp
@@ -0,0 +1,14 @@
+//
+//  Stack.hpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 2/22/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Stack_hpp
+#define Stack_hpp
+
+#include <stdio.h>
+
+#endif /* Stack_hpp */
