7a5215e9961717ba3dc6e3d46e0248ca13bcc820
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-26 13:09:40 -0700

    getFrom and remove methods

--- DataStructureProject/Model/Structures/Linear/CircularList.hpp
+++ DataStructureProject/Model/Structures/Linear/CircularList.hpp
@@ -114,5 +114,42 @@ void CircularList<Type> :: addAtIndex(int index, Type item)
     next->setPrevious(addme);
     this->size++;
 }
+
+template <class Type>
+Type circularList<Type> :: getFromIndex(int index)
+{
+    assert (index >= 0 && index < this->size);
+    DoubleNode<Type> * holder = findNode(index);
+    return holder->getData();
+}
+
+template <class Type>
+Type CircularList<Type> :: remove(int index)
+{
+    assert (index >= 0 && index < this->size);
+    
+    DoubleNode<Type> * removed = findNode(index);
+    DoubleNode<Type> * removedPrevious = removed->getPrevious();
+    DoubleNode<Type> * removedNext = removed->getNext();
+    
+    if (index ==0)
+    {
+        this->front = removedNext;
+        this->end->setNext(removedNext);
+    }
+    if (index == this->size - 1)
+    {
+        this->end = removedPrevious;
+        this->front->setPrevious(removedPrevious);
+    }
+    
+    removedPrevious->setNext(removedNext);
+    removedNext->setPrevious(removedPrevious);
+    
+    Type value = removed ->getData();
+    this->size--;
+    delete removed;
+    return value;
+}
                                                      
 #endif /* CircularList_hpp */
