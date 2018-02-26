241e8be530ba41dc34a2610d7416d7cc9755caef
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-26 11:56:28 -0700

    Created the DoubleNode class, added inheritence and class header

---
+++ DataStructureProject/Model/Nodes/DoubleNode.hpp
@@ -0,0 +1,20 @@
+//
+//  DoubleNode.hpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef DoubleNode_hpp
+#define DoubleNode_hpp
+
+#include <Node.hpp>
+
+
+template <class Type>
+class DoubleNode : public Node<Type>
+{
+    
+};
+#endif /* DoubleNode_hpp */
