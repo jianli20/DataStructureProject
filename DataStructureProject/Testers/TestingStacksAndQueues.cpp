a3641f924767e39759fa22a3ed87c31094203ada
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-02-26 12:21:50 -0700

    added includes and class code

---
+++ DataStructureProject/Testers/TestingStacksAndQueues.cpp
@@ -0,0 +1,10 @@
+
+//
+//  TestingStacksAndQueues.cpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "TestingStacksAndQueues.hpp"
---
+++ DataStructureProject/Testers/TestingStacksAndQueues.hpp
@@ -0,0 +1,28 @@
+//
+//  TestingStacksAndQueues.hpp
+//  DataStructureProject
+//
+//  Created by Li, Jian Hong on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef TestingStacksAndQueues_hpp
+#define TestingStacksAndQueues_hpp
+
+#include "../Controller/Tools/Timer.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Model/Structures/Linear/Queue.hpp"
+#include "../Model/Structures/Linear/Stack.hpp"
+#include "../Model/Structures/Linear/List.hpp"
+#include <iostream>
+using namespace std;
+
+class TestingStacksAndQueues
+{
+public:
+    void stackVersusList;
+    void stackVersusQueue;
+    void queueVersusList;
+};
+
+#endif /* TestingStacksAndQueues_hpp */
