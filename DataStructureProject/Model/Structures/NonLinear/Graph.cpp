fc45dab21b40ddf8c6bf83054d6a857218e70c98
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-03-19 13:47:47 -0600

    All the methods for graph class

--- DataStructureProject/Model/Structures/NonLinear/Graph.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Graph.cpp
-//  DataStructureProject
-//
-//  Created by Li, Jian Hong on 3/19/18.
-//  Copyright © 2018 CTEC. All rights reserved.
-//
-
-#include "Graph.hpp"
--- DataStructureProject/Model/Structures/NonLinear/Graph.hpp
+++ DataStructureProject/Model/Structures/NonLinear/Graph.hpp
@@ -19,7 +19,7 @@ class Graph
 {
 private: static const int MAXIMUM = 20;
     bool adjacencyMatrix [MAXIMUM][MAXIMUM];
-    int weightCostMaxtrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
     Type graphData[MAXIMUM];
     int vertexCount;
     void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
@@ -46,7 +46,7 @@ public:
     //Check connections
     bool hasUndirectedConnection(int source, int target) const;
     bool areConnected(int source, int target)const;
-    std::set<int> neightbors(int vertex) const;
+    std::set<int> neighbors(int vertex) const;
     
     //Traversals
     void depthFirstTraversal(Graph<Type> & graph, int vertex);
@@ -91,4 +91,163 @@ Type Graph<Type> :: operator[](int vertex) const
     assert(vertex < vertexCount);
     return graphData[vertex];
 }
+
+template <class Type>
+void Graph<Type> :: addVertex(const Type& value)
+{
+    assert(vertexCount < MAXIMUM);
+    int newVertexNumber = vertexCount;
+    vertexCount++;
+    
+    for(int otherVertexNumber = 0; otherVertexNumber < vertexCount; otherVertexNumber++)
+    {
+        adjacencyMatrix[otherVertexNumber][newVertexNumber] = false;
+        adjacencyMatrix[newVertexNumber][otherVertexNumber] = false;
+    }
+    
+    graphData[newVertexNumber] = value;
+}
+
+template <class Type>
+void Graph <Type> :: removeEdge(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target>= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = false;
+}
+
+template <class Type>
+void Graph<Type> :: removeEdgeUndirected(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = false;
+    adjacencyMatrix[target][source] = false;
+}
+
+template <class Type>
+void Graph<Type> :: removeEdgeCost(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    weightCostMatrix[source][target] = 0;
+    weightCostMatrix[target][source] = 0;
+}
+
+template<class Type>
+void Graph<Type> :: addEdge(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = true;
+}
+
+template <class Type>
+void Graph<Type> :: addEdgeCost(int source, int target, int cost)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    weightCostMatrix[source][target] = cost;
+    weightCostMatrix[target][source] = cost;
+}
+
+template <class Type>
+void Graph<Type> :: addEdgeUndirected(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = true;
+    adjacencyMatrix[target][source] = true;
+}
+
+bool Graph<Type> :: hasUndirectConnection(int source, int target) const
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source][target] ;
+    
+    return isAnEdge;
+
+}
+
+template <class Type>
+bool Graph<Type> :: areConnected(int source, int target) const
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source][target];
+    
+    return isAnEdge;
+}
+
+template <class Type>
+std::set<int> Graph<Type> :: neighbors(int vertex) const
+{
+    assert(vertex < vertexCount);
+    std::set<int> vertexNeighbors;
+    
+    for(int index = 0; index < vertexCount; index++)
+    {
+        if(adjacencyMatrix[vertex][index])
+        {
+            vertexNeighbors.insert(index);
+        }
+        return vertexNeighbors;
+}
+}
+
+template  <class Type>
+void Graph<Type> :: depthFirstTraversal(Graph<Type> & currentGraph, int vertex)
+{
+    bool visitedVertices[MAXIMUM];
+    assert(vertex < currentGraph.size());
+    std::fill_n(visitedVertices, currentGraph.size(), false);
+    depthFirstTraversal(currentGraph, vertex, visitedVertices);
+}
+
+template <class Type>
+void Graph<Type> :: depthFirstTraversal(Graph<Type> & currentGraph, int vertex, bool * visited)
+{
+    std::set<int> connections = currentGraph.neighbors(vertex);
+    std::set<int>::iterator setIterator;
+    
+    visited[vertex] = true;
+    cout << currentGraph[vertex] << ", " << endl;
+    
+    for(setIterator = connections.begin(); setIterator != connections.end(); setIterator++)
+    {
+        if(!visited[*setIterator])
+        {
+            depthFirstTraversal(currentGraph, *setIterator, visited);
+        }
+}
+    
+template <class Type.
+    int Graph<Type> :: costTraversal(Graph<Type> & currentGraph, int vertex)
+    {
+        assert(vertex < currentGraph.size());
+        int cost = 0;
+        bool visited[MAXIMUM];
+    std:set<int> connections;
+    std:set<int>::iterator setIterator;
+    std:queue<int> vertexQueue;
+        
+        std::fill_n(visited,currentGraph.size(),false);
+        visited[vertex] = true;
+        
+        vertexQueue.push(vertex);
+        while (!vertexQueue.empty())
+        {
+            connections = currentGraph.neighbors(vertexQueue.front());
+            vertexQueue.pop();
+            
+            for(setIterator = connections.begin(); setIterator != connections.end(); setIterator++)
+            {
+                if(!visited[*setIterator])
+                    {
+                        cost += weightCostMaxtrix[vertex][*setIterator];
+                        visited[*setIterator] = true;
+                        verexQueue.push(*setIterator);
+                     }
+            }
+        }
+    }
+}
+
 #endif /* Graph_hpp */
