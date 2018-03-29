84be99c307805828022a3faecb661a47b6bff458
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-03-29 13:43:26 -0600

    BinaryTreeTester

--- DataStructureProject/Testers/BinaryTreeTester.cpp
+++ DataStructureProject/Testers/BinaryTreeTester.cpp
@@ -2,8 +2,27 @@
 //  BinaryTreeTester.cpp
 //  DataStructureProject
 //
-//  Created by Li, Jian Hong on 3/27/18.
+//  Created by Li, Jian Hong on 3/29/18.
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
 #include "BinaryTreeTester.hpp"
+void BinaryTreeTester :: doTreeStuff()
+{
+    testTree.insert(3);
+    testTree.insert(1);
+    testTree.insert(2);
+    testTree.insert(-3246000);
+    testTree.insert(-12);
+    testTree.insert(7);
+    testTree.insert(5);
+    testTree.insert(6);
+    testTree.insert(8);
+    testTree.insert(5000);
+    testTree.insert(9001);
+    testTree.insert(10);
+    testTree.insert(600);
+    
+    
+    testTree.inOrderTraversal();
+}
--- DataStructureProject/Testers/BinaryTreeTester.hpp
+++ DataStructureProject/Testers/BinaryTreeTester.hpp
@@ -2,7 +2,7 @@
 //  BinaryTreeTester.hpp
 //  DataStructureProject
 //
-//  Created by Li, Jian Hong on 3/27/18.
+//  Created by Li, Jian Hong on 3/29/18.
 //  Copyright © 2018 CTEC. All rights reserved.
 //
 
@@ -10,13 +10,15 @@
 #define BinaryTreeTester_hpp
 
 #include "../Model/Nodes/BinaryTreeNode.hpp"
+#include "../Model/Structures/NonLinear/BinarySearchTree.hpp"
 #include <iostream>
 
 class BinaryTreeTester
 {
 private:
-    
+    BinarySearchTree<int> testTree;
 public:
+    void doTreeStuff();
 };
 
 #endif /* BinaryTreeTester_hpp */
