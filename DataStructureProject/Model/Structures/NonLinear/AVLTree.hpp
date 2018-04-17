2a959e4877631d552aac55cbec0e54440ed7292b
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-04-17 12:27:39 -0600

    insert() and remove()

--- DataStructureProject/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructureProject/Model/Structures/NonLinear/AVLTree.hpp
@@ -124,6 +124,20 @@ BinaryTreeNode<Type> * AVLTree<Tpye :: balanceSubTree (BinaryTreeNode<Type> * pa
     
     return parent;
 }
+
+template <class Type>
+void AVLTree<Type> :: insert(Type item)
+{
+    insertNode(this->getRoot(), item);
+}
+
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    removeNode(this->getRoot(), item);
+}
+
+
 }
 
 #endif /* AVLTree_hpp */
